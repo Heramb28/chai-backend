4414761190a1371b21df37f944c4469099d0a4f3
Heramb Sharma <sharma.heramb28@gmail.com>
2024-05-19 06:20:29 +0530

    shh

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,7 +13,6 @@ const registerUser = asyncHandler ( async (req,res) => {
     //  remove password and refresh token field from response
     //  check for user creation
     //  return response
-
     const {fullName, email, username, password} = req.body
     console.log("email:", email);
     if (
@@ -23,6 +22,7 @@ const registerUser = asyncHandler ( async (req,res) => {
     ) {
         throw new ApiError(400,"All field are required")
     }
+    
 
     const existedUser = User.findOne({
         $or: [{ username } , { email }]

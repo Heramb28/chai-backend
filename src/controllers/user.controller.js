1645436b4fbc92833edccc46c41c56742220597c
Heramb Sharma <sharma.heramb28@gmail.com>
2024-05-24 00:03:20 +0530

    push

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -78,4 +78,11 @@ const registerUser = asyncHandler ( async (req,res) => {
 
 })
 
-export {registerUser}
+const loginUser = asyncHandler( async (req,res) =>{
+
+} )
+
+export {
+    registerUser,
+    loginUser
+}
